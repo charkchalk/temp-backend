750ac0f0182f65aa5890a4a18a47d6946c0b36ef
moontai0724 <moontai0724@gmail.com>
2023-05-23 12:15:34 +0800

    feat: keyword search for organization

--- src/controllers/organization.ts
+++ src/controllers/organization.ts
@@ -6,12 +6,32 @@ import type Paginated from "../types/paginated";
 
 export default async function OrganizationRoute(fastify: FastifyInstance) {
   fastify.get<{
-    Querystring: PaginateOptions;
+    Querystring: PaginateOptions & {
+      keyword?: string;
+    };
   }>("/", async function getAll(req): Promise<Paginated> {
     const page = Number(req.query.page || 1);
     const size = Number(req.query.size || 25);
+    const keyword = req.query.keyword || "";
 
-    return OrganizationService.getAll(fastify, { page, size });
+    return OrganizationService.getAll(fastify, {
+      page,
+      size,
+      where: {
+        OR: [
+          {
+            name: {
+              contains: keyword,
+            },
+          },
+          {
+            description: {
+              contains: keyword,
+            },
+          },
+        ],
+      },
+    });
   });
 
   fastify.get<{
