1857ec5f4376707b656a3b6dbc1cbe3119f04e95
moontai0724 <moontai0724@gmail.com>
2023-05-24 14:04:17 +0800

    feat: course search api

--- src/controllers/course.ts
+++ src/controllers/course.ts
@@ -1,6 +1,8 @@
+import type { Prisma } from "@prisma/client";
 import type { FastifyInstance } from "fastify";
 
 import CourseService from "../services/course.service";
+import TimeRangeService from "../services/time-range.service";
 import type PaginateOptions from "../types/paginate-options";
 import type Paginated from "../types/paginated";
 
@@ -29,4 +31,247 @@ export default async function CourseRoute(fastify: FastifyInstance) {
 
     return course;
   });
+
+  fastify.post<{
+    Querystring: PaginateOptions;
+    Body: SerachBody[];
+  }>("/search", async function search(req, res) {
+    const page = Number(req.query.page || 1);
+    const size = Number(req.query.size || 25);
+    const where = {
+      AND: [],
+    };
+
+    await Promise.all(
+      req.body.map(async ({ key, method, value: values }) => {
+        const OR: Prisma.CourseWhereInput["OR"] = [];
+
+        switch (key) {
+          case "code":
+            values.forEach(value => {
+              if (method === "=")
+                OR.push({
+                  code: {
+                    contains: value,
+                  },
+                });
+              else if (method === "!=")
+                OR.push({
+                  code: {
+                    not: {
+                      contains: value,
+                    },
+                  },
+                });
+            });
+            break;
+          case "credit":
+            values.forEach(value => {
+              if (method === "=")
+                OR.push({
+                  credit: {
+                    equals: Number(value),
+                  },
+                });
+              else if (method === "!=")
+                OR.push({
+                  credit: {
+                    not: {
+                      equals: Number(value),
+                    },
+                  },
+                });
+            });
+            break;
+          case "dateRange":
+            values.forEach(value => {
+              if (method === "=")
+                OR.push({
+                  dateRange: {
+                    uuid: value,
+                  },
+                });
+              else if (method === "!=")
+                OR.push({
+                  dateRange: {
+                    isNot: {
+                      uuid: value,
+                    },
+                  },
+                });
+            });
+            break;
+          case "host":
+            values.forEach(value => {
+              if (method === "=")
+                OR.push({
+                  hosts: {
+                    some: {
+                      uuid: value,
+                    },
+                  },
+                });
+              else if (method === "!=")
+                OR.push({
+                  hosts: {
+                    none: {
+                      uuid: value,
+                    },
+                  },
+                });
+            });
+            break;
+          case "keyword":
+            values.forEach(value => {
+              if (method === "=") {
+                OR.push({
+                  name: {
+                    contains: value,
+                  },
+                });
+                OR.push({
+                  description: {
+                    contains: value,
+                  },
+                });
+              } else if (method === "!=") {
+                OR.push({
+                  NOT: {
+                    name: {
+                      contains: value,
+                    },
+                  },
+                });
+                OR.push({
+                  NOT: {
+                    description: {
+                      contains: value,
+                    },
+                  },
+                });
+              }
+            });
+            break;
+          case "organization":
+            values.forEach(value => {
+              if (method === "=")
+                OR.push({
+                  organization: {
+                    uuid: value,
+                  },
+                });
+              else if (method === "!=")
+                OR.push({
+                  organization: {
+                    NOT: {
+                      uuid: value,
+                    },
+                  },
+                });
+            });
+            break;
+          case "place":
+            values.forEach(value => {
+              if (method === "=")
+                OR.push({
+                  places: {
+                    some: {
+                      uuid: value,
+                    },
+                  },
+                });
+              else if (method === "!=")
+                OR.push({
+                  places: {
+                    none: {
+                      uuid: value,
+                    },
+                  },
+                });
+            });
+            break;
+          case "timeRange":
+            await Promise.all(
+              values.map(async value => {
+                const destructedTimeRanges =
+                  TimeRangeService.destructRange(value);
+                const timeRanges = await TimeRangeService.getRanges(
+                  fastify,
+                  destructedTimeRanges,
+                );
+
+                if (method === "=") {
+                  OR.push({
+                    timeRanges: {
+                      some: {
+                        id: {
+                          in: timeRanges.map(({ id }) => id),
+                        },
+                      },
+                    },
+                  });
+                } else if (method === "!=") {
+                  OR.push({
+                    timeRanges: {
+                      none: {
+                        id: {
+                          in: timeRanges.map(({ id }) => id),
+                        },
+                      },
+                    },
+                  });
+                }
+              }),
+            );
+            break;
+        }
+
+        (where.AND as Prisma.CourseWhereInput[]).push({ OR });
+      }),
+    );
+
+    const courses = await CourseService.getAll(fastify, {
+      page,
+      size,
+      where,
+    });
+
+    return courses;
+  });
+}
+
+type SerachBody =
+  | CodeSearchBody
+  | CreditSearchBody
+  | DateRangeSearchBody
+  | HostSearchBody
+  | KeywordSearchBody
+  | OrganizationSearchBody
+  | PlaceSearchBody
+  | TimeRangeSearchBody;
+
+interface BasicSearchBody<Key extends string, Value = string[]> {
+  key: Key;
+  method: "=" | "!=";
+  value: Value;
+}
+
+type CodeSearchBody = BasicSearchBody<"code", string[]>;
+type CreditSearchBody = BasicSearchBody<"credit", string[]>;
+type DateRangeSearchBody = BasicSearchBody<"dateRange", string[]>;
+type HostSearchBody = BasicSearchBody<"host", string[]>;
+type KeywordSearchBody = BasicSearchBody<"keyword", string[]>;
+type OrganizationSearchBody = BasicSearchBody<"organization", string[]>;
+type PlaceSearchBody = BasicSearchBody<"place", string[]>;
+type TimeRangeSearchBody = BasicSearchBody<"timeRange", ITimeRange[]>;
+
+export interface ITimeRange {
+  start: {
+    day: number;
+    time: string;
+  };
+  end: {
+    day: number;
+    time: string;
+  };
 }
--- src/services/time-range.service.ts
+++ src/services/time-range.service.ts
@@ -1,11 +1,13 @@
 import type { Prisma, TimeRange } from "@prisma/client";
+import { Weekday } from "@prisma/client";
 import type { FastifyInstance } from "fastify";
 
+import { ITimeRange } from "../controllers/course";
 import type PaginateOptions from "../types/paginate-options";
 import type Paginated from "../types/paginated";
 import { getTime } from "../utils/date-time";
 
-const TimeRangeService = { getAll, getOne, toPublic };
+const TimeRangeService = { getAll, getOne, toPublic, destructRange, getRanges };
 export default TimeRangeService;
 
 export async function toPublic(timeRange: TimeRange): Promise<PublicTimeRange> {
@@ -38,6 +40,111 @@ async function getAll(
     currentPage: page,
     content: await Promise.all(exposedTimeRanges),
   };
+}
+
+const Weekdays = [
+  "MONDAY",
+  "TUESDAY",
+  "WEDNESDAY",
+  "THURSDAY",
+  "FRIDAY",
+  "SATURDAY",
+  "SUNDAY",
+] as Weekday[];
+
+interface RawTimeRange {
+  weekday: Weekday;
+  startTime: string;
+  endTime: string;
+}
+
+function destructRange(range: ITimeRange) {
+  const ranges: RawTimeRange[] = [];
+
+  if (range.start.day < range.end.day) {
+    for (let i = range.start.day; i <= range.end.day; i++) {
+      if (i === range.start.day) {
+        ranges.push({
+          weekday: Weekdays[i - 1],
+          startTime: range.start.time,
+          endTime: "23:59",
+        });
+      } else if (i === range.end.day) {
+        ranges.push({
+          weekday: Weekdays[i - 1],
+          startTime: "00:00",
+          endTime: range.end.time,
+        });
+      } else {
+        ranges.push({
+          weekday: Weekdays[i - 1],
+          startTime: "00:00",
+          endTime: "23:59",
+        });
+      }
+    }
+  } else if (range.start.day > range.end.day) {
+    for (let i = range.start.day; i <= 7; i++) {
+      if (i === range.start.day) {
+        ranges.push({
+          weekday: Weekdays[i - 1],
+          startTime: range.start.time,
+          endTime: "23:59",
+        });
+      } else {
+        ranges.push({
+          weekday: Weekdays[i - 1],
+          startTime: "00:00",
+          endTime: "23:59",
+        });
+      }
+    }
+
+    for (let i = 1; i <= range.end.day; i++) {
+      if (i === range.end.day) {
+        ranges.push({
+          weekday: Weekdays[i - 1],
+          startTime: "00:00",
+          endTime: range.end.time,
+        });
+      } else {
+        ranges.push({
+          weekday: Weekdays[i - 1],
+          startTime: "00:00",
+          endTime: "23:59",
+        });
+      }
+    }
+  } else {
+    ranges.push({
+      weekday: Weekdays[range.start.day - 1],
+      startTime: range.start.time,
+      endTime: range.end.time,
+    });
+  }
+
+  return ranges;
+}
+
+async function getRanges(fastify: FastifyInstance, range: RawTimeRange[]) {
+  const allTimeRanges: TimeRange[] = [];
+
+  for await (const { weekday, startTime, endTime } of range) {
+    const timeRanges = await fastify.prisma.$queryRawUnsafe<TimeRange[]>(
+      "SELECT * FROM `TimeRange` WHERE (`weekday` = ? AND `startTime` >= ? AND `endTime` <= ?)",
+      weekday,
+      `1970-01-01T${startTime}:00.000Z`,
+      `1970-01-01T${endTime}:00.000Z`,
+    );
+
+    allTimeRanges.push(...timeRanges);
+  }
+
+  const r = allTimeRanges.filter(
+    (value, index, array) => array.indexOf(value) === index,
+  );
+
+  return r;
 }
 
 async function getOne(
